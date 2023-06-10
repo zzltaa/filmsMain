8444ff750734050922a18708f7fa51ed1445d8d4
zzltaa <131349262+zzltaa@users.noreply.github.com>
2023-06-10 09:22:41 +0300

    Update main.jsx
    проект

--- src/layout/main.jsx
+++ src/layout/main.jsx
@@ -10,14 +10,14 @@ class Main extends React.Component{
         loading: true,
     };
     componentDidMount() {
-        fetch('http://www.omdbapi.com/?apikey=fb236814&s=mario')
+        fetch('https://www.omdbapi.com/?apikey=fb236814&s=mario')
             .then((response) => response.json())
             .then((data) => this.setState({movies:data.Search, loading: false}));
     }
     searchMovies = (str, type = "all") => {
         this.setState({ loading: true })
         fetch(
-            `http://www.omdbapi.com/?apikey=fb236814&s=${str}${
+            `https://www.omdbapi.com/?apikey=fb236814&s=${str}${
                 type !== "all" ? `&type=${type}` : ""
             }`
         )
